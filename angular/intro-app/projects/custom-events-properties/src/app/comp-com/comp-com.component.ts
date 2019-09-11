d9c5610b5a51200caaea2ad0c4a28ae032482f83
aniruddha <aniruddha.kudalkar@gmail.com>
2019-09-11 09:37:05 +0530

    component : inter component communication

--- angular/intro-app/projects/custom-events-properties/src/app/comp-com/comp-com.component.ts
+++ angular/intro-app/projects/custom-events-properties/src/app/comp-com/comp-com.component.ts
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CompComComponent implements OnInit {
 
+  curPrg = 50
   constructor() { }
 
   ngOnInit() {
--- angular/intro-app/projects/custom-events-properties/src/app/comp-com/prg.component.ts
+++ angular/intro-app/projects/custom-events-properties/src/app/comp-com/prg.component.ts
@@ -1,15 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-prg',
   template: `
     <div class="progress">
-      <div class="progress-bar" role="progressbar" style="width: 25%;" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100">25%</div>
+      <div 
+        class="progress-bar" 
+        role="progressbar" 
+        [style.width.%]="wd" 
+        [attr.aria-valuenow]="wd" 
+        aria-valuemin="0" 
+        aria-valuemax="100">
+          {{wd}}%
+        </div>
     </div>
   `,
   styles: []
 })
 export class PrgComponent implements OnInit {
+
+  @Input()
+  wd: number;
 
   constructor() { }
 
