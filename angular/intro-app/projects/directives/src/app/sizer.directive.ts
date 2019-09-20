2cb13693900a794ce37d8c4376a24f780dce71a5
aniruddha <aniruddha.kudalkar@gmail.com>
2019-09-20 10:53:32 +0530

    directives : renders

--- angular/intro-app/projects/directives/src/app/sizer.directive.ts
+++ angular/intro-app/projects/directives/src/app/sizer.directive.ts
@@ -1,22 +1,33 @@
-import { Directive, HostBinding, HostListener, Input } from '@angular/core';
+import { Directive, HostBinding, HostListener, Input, OnInit, ElementRef, Renderer2 } from '@angular/core';
 
 @Directive({
   selector: '[appSizer]'
 })
-export class SizerDirective {
+export class SizerDirective implements OnInit {
 
-  @HostBinding('style.width.px')
+  // @HostBinding('style.width.px')
   @Input('appSizer')
-  wd : number
+  wd: number
 
-  constructor() { }
+  constructor(
+    private elRf: ElementRef<HTMLElement>,
+    private rend: Renderer2
+  ) { }
 
-  @HostListener('mouseenter')
+  ngOnInit() {
+    // this.elRf.nativeElement.style.border = '1px solid red' // usecured
+    this.rend.setStyle(this.elRf.nativeElement, 'border', '1px solid red') //secured
+    this.rend.listen(this.elRf.nativeElement, 'onclick', () => {
+      this.rend.setStyle(this.elRf.nativeElement, 'border', '2px solid red') //secured
+    })
+  }
+
+  // @HostListener('mouseenter')
   onIn() {
     this.wd = 600
   }
 
-  @HostListener('mouseleave')
+  // @HostListener('mouseleave')
   onOut() {
     this.wd = 300
   }
