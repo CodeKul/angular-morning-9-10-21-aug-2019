4dba12868724956baf4bc7f3b0f92cb2c0b656e2
aniruddha <aniruddha.kudalkar@gmail.com>
2019-09-23 10:22:38 +0530

    service : intro and setup

---
+++ angular/intro-app/projects/services/src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component, Injectable } from '@angular/core';
+import { MyService } from './my-service.service';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+  providers : [MyService]
+})
+export class AppComponent {
+  title = 'services';
+}
---
+++ angular/intro-app/projects/services/src/app/app.module.ts
@@ -0,0 +1,17 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { MyService } from './my-service.service'
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [ MyService ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
