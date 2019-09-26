c09a7a9684f1ad7a8bd1c089caa5fd293eeb37c1
aniruddha <aniruddha.kudalkar@gmail.com>
2019-09-26 10:33:37 +0530

    routers : route configuration

---
+++ angular/intro-app/projects/routers/src/app/app-routing.module.ts
@@ -0,0 +1,17 @@
+import { DashComponent } from './dash/dash.component';
+import { ChatComponent } from './chat/chat.component';
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+
+const routes: Routes = [
+  { path: '', component: LoginComponent },
+  { path: 'chat', component: ChatComponent },
+  { path: 'dash', component: DashComponent },
+  { path: '**', redirectTo: '' }
+];
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
