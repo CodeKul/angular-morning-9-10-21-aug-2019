1196a2bb1f848004432512820987f44d05ad2f38
aniruddha <aniruddha.kudalkar@gmail.com>
2019-09-28 10:52:04 +0530

    forms : td : configuration and setup

---
+++ angular/intro-app/projects/forms/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { FormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { TemplateDrivenComponent } from './template-driven/template-driven.component';
+import { DataDrivenComponent } from './data-driven/data-driven.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    TemplateDrivenComponent,
+    DataDrivenComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
